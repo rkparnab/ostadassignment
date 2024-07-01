1829ee3d7b78efdd830d6b00cae512f7e61668ee
rkparnab <141383121+rkparnab@users.noreply.github.com>
2024-07-01 18:02:51 +0530

    Add files via upload

---
+++ celsius_to_farenheit.php
@@ -0,0 +1,17 @@
+<?php
+
+$temperature_celsius = 32;
+
+$temperature_farenheit = ( $temperature_celsius * 9 / 5 ) + 32;
+
+printf( "The Farenheit Temperature is %.2f", $temperature_farenheit );
+
+
+
+
+
+
+
+
+
+
---
+++ largest_number.php
@@ -0,0 +1,24 @@
+<?php
+
+$num1 = 4;
+ $num2 = 5;
+   $num3 = 6;
+
+     if ($num1 >= $num2 && $num1 >= $num3) {
+              echo "This is the largest number $num1";
+
+           } elseif ($num2 >= $num1 && $num2 >= $num3) {
+                  echo "This is the largest number $num2";
+                  
+                   } else {
+                        echo "This is the largest number $num3";
+                       }
+
+
+
+
+
+
+
+
+
